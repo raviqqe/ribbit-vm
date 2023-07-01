03119e38b4cd74ac40811d5e0c93b0b6b958c8d1
Yota Toyama <raviqqe@gmail.com>
2023-07-01 16:17:12 +1000

    Fix

--- command/src/main.rs
+++ command/src/main.rs
@@ -1,3 +1,12 @@
+use std::process::exit;
+use vm::{Error, Vm};
+
+const INPUT: &[u8] = b");'u?>vD?>vRD?>vRA?>vRA?>vR:?>vR=!(:lkm!':lkv6y";
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = Vm::new(INPUT).run() {
+        exit(match error {
+            Error::IllegalInstruction | Error::IllegalPrimitive => 6,
+        })
+    }
 }
